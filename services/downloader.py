27a9640d23029a3f9894634f40660819e2db0daf
Isabella Medeiros <isabellamedeirosreal@gmail.com>
2025-11-23 21:13:33 -0300

    debug 2

--- services/downloader.py
+++ services/downloader.py
@@ -3,36 +3,32 @@ import os
 import time
 
 def download_video(url):
-    # cria pasta se não existir
     if not os.path.exists('downloads'):
         os.makedirs('downloads')
 
-    nome_arquivo = f"video_{int(time.time())}"
-    
-    # caminho do arquivo de cookies
-    # o bot vai procurar o arquivo 'cookies.txt' na mesma pasta onde ele está rodando
+    nome_arquivo = f"midia_{int(time.time())}"
     arquivo_cookies = 'cookies.txt'
 
     options = {
-        'format': 'best', 
-        
+        'format': 'bestvideo+bestaudio/best',
+        'merge_output_format': 'mp4',
         'outtmpl': f'downloads/{nome_arquivo}.%(ext)s',
         'noplaylist': True,
         'quiet': True,
-        'writethumbnail': False, # garante que não baixe miniaturas extras
+        'writethumbnail': False,
         'cookiefile': arquivo_cookies,
     }
-    
-    # verificação de segurança: se o arquivo cookies.txt não existir, avisa no terminal
-    if not os.path.exists(arquivo_cookies):
-        print(f"AVISO: O arquivo '{arquivo_cookies}' não foi encontrado! O TikTok pode falhar.")
 
     try:
         with yt_dlp.YoutubeDL(options) as ydl:
             info = ydl.extract_info(url, download=True)
             filename = ydl.prepare_filename(info)
+            if options.get('merge_output_format') == 'mp4' and not filename.endswith('.mp4'):
+                pre, ext = os.path.splitext(filename)
+                filename = pre + ".mp4"
+                
             return filename
-        
+            
     except Exception as e:
         print(f"Erro no download: {e}")
         return None
