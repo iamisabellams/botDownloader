9875be3510878a5eeb0e38279f9d1a499baea180
Isabella Medeiros <isabellamedeirosreal@gmail.com>
2025-11-27 16:44:38 -0300

    desativando cookies para o youtube

--- services/downloader.py
+++ services/downloader.py
@@ -5,8 +5,12 @@ import time
 def download_video(url):
     if not os.path.exists('downloads'):
         os.makedirs('downloads')
+
     nome_base = f"video_{int(time.time())}"
     arquivo_cookies = 'cookies.txt'
+    usar_cookies = True
+    if "youtube.com" in url or "youtu.be" in url:
+        usar_cookies = False
 
     options = {
         'format': 'bestvideo+bestaudio/best',
@@ -15,14 +19,15 @@ def download_video(url):
         'noplaylist': True,
         'quiet': True,
         'writethumbnail': False,
-        'cookiefile': arquivo_cookies,
+        'cookiefile': arquivo_cookies if usar_cookies else None,
     }
 
     try:
         with yt_dlp.YoutubeDL(options) as ydl:
             info = ydl.extract_info(url, download=True)
             filename = ydl.prepare_filename(info)
-            if options.get('merge_output_format') == 'mp4':
+            
+            if options.get('merge_output_format') == 'mp4' and not filename.endswith('.mp4'):
                 base, ext = os.path.splitext(filename)
                 return base + ".mp4"
                 
